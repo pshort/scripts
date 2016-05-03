06bc571d5179de453a9f06fd7b0ce4c40170f9ea
Peter Short <peter.short@gmail.com>
2016-05-03 10:03:21 +0000

    Creating scripts repo

---
+++ scripts/init.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+sudo apt-get install git -y
+sudo apt-get install vim -y
+
+git config --global user.email "peter.short@gmail.com"
+git config --global user.name "Peter Short"
