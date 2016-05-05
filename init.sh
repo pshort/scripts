38d85d5878acbc260e3f64d442db2e7875beeb56
Peter Short <peter.short@gmail.com>
2016-05-05 19:52:32 +0000

    made script work for non debian rpm yum

--- init.sh
+++ init.sh
@@ -1,10 +1,23 @@
+YUM_CMD=$(which yum)
+APT_GET_CMD=$(which apt-get)
 
-sudo apt-get update -y
-sudo apt-get upgrade -y
+if [[ ! -z $YUM_CMD]]; then
 
-sudo apt-get install git -y
-sudo apt-get install vim -y
-sudo apt-get install tmux -y
+    sudo yum update -y
+
+    sudo yum install git -y
+    sudo yum install vim -y
+    sudo yum install tmux -y
+
+elif [[ ! -z $APT_GET_CMD]]; then
+
+    sudo apt-get update -y
+    sudo apt-get upgrade -y
+
+    sudo apt-get install git -y
+    sudo apt-get install vim -y
+    sudo apt-get install tmux -y
+fi
 
 git config --global user.email "peter.short@gmail.com"
 git config --global user.name "Peter Short"
