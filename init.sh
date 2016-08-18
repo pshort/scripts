697a675792c09c5f68404d3850f163f6132903e3
Peter Short <peter.short@gmail.com>
2016-08-18 13:08:25 +0000

    keyrings on init + plugins for vim installed + ycm + rubygems fix

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ if [[ ! -z $YUM_CMD ]]; then
     sudo yum install git -y
     sudo yum install vim -y
     sudo yum install tmux -y
-    sudo yum install gem -y
+    sudo yum install rubygems -y
 
 elif [[ ! -z $APT_GET_CMD ]]; then
 
@@ -18,7 +18,7 @@ elif [[ ! -z $APT_GET_CMD ]]; then
     sudo apt-get install git -y
     sudo apt-get install vim -y
     sudo apt-get install tmux -y
-    sudo apt-get install gem -y
+    sudo apt-get install rubygems -y
 else
     echo "Cant find a package manager :("
     exit 1;
@@ -34,8 +34,22 @@ cp .vimrc ~/.vimrc
 cp .bashrc ~/.bashrc
 source ~/.bashrc
 
+# install vundle plugins
+vim +PluginInstall +qall
+
+# you complete me
+cd ~/.vim/bundle/YouCompleteMe
+./install.py
+
 mkdir ~/.tmuxinator
 cp ping.yml ~/.tmuxinator/ping.yml
 
+#--------------------------- GIT KEYRING ------------------------
+
+sudo apt-get install libgnome-keyring-dev
+sudo make --directory=/usr/share/doc/git/contrib/credential/gnome-keyring
+git config --global credential.helper /usr/share/doc/git/contrib/credential/gnome-keyring/git-credential-gnome-keyring
+
+#----------------------------------------------------------------
 
 sudo gem install tmuxinator
